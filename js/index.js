19072ed49a3e1405e8acd0b79a986ed9b63b86e7
Vih <64778152+Taidokas95@users.noreply.github.com>
2020-08-20 15:14:46 +0200

    Style changes

--- js/index.js
+++ js/index.js
@@ -1,53 +1,44 @@
 //navigation
 var about = document.getElementById("about");
-
 function scrollToAbout() {
   about.scrollIntoView(true); 
 }
 
 var schedule = document.getElementById("schedule");
-
 function scrollToSchedule() {
   schedule.scrollIntoView(true); 
 }
 
 var venue = document.getElementById("venue");
-
 function scrollToVenue() {
   venue.scrollIntoView(true); 
 }
 
 var hotel = document.getElementById("hotel");
-
 function scrollToHotel() {
   hotel.scrollIntoView(true); 
 }
 
 var travel = document.getElementById("travel");
-
 function scrollToTravel() {
   travel.scrollIntoView(true); 
 }
 
 var tourism = document.getElementById("tourism");
-
 function scrollToTourism() {
   tourism.scrollIntoView(true); 
 }
 
 var gifts = document.getElementById("gifts");
-
 function scrollToGifts() {
   gifts.scrollIntoView(true); 
 }
 
 var schedule = document.getElementById("RSVP");
-
 function scrollToRSVP() {
   RSVP.scrollIntoView(true); 
 }
 var contactInfo = document.getElementById("contactInfo");
-
 function scrollToContactInfo() {
   contactInfo.scrollIntoView(true); 
 }
